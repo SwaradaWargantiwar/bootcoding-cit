bcdc7ba9bd343604b95304e87566d29ac7e45687
SwaradaWargantiwar <swaradawargantiwar25@gmail.com>
2023-08-18 12:37:38 +0530

    java program

---
+++ src/java/thirdprogramjava.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class thirdprogramjava {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System .in);
+        System.out.println("Enter First Number:");
+        int n= sc. nextInt();
+        System.out.println("Enter Second Number:");
+        int m = sc.nextInt();
+       // int add = n + m;
+        //System.out.println("Addition=" + add);
+
+
+
+        //int mul = n * m;
+       // System.out.println(mul);
+
+        int div = n/m;
+      System.out.println(div);
+    }
+
+}
